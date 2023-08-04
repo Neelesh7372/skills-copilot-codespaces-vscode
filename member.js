ab1f521fa58410699515f9e1864457353bc00456
NEELESH KUMAR SHARMA <79575531+Neelesh7372@users.noreply.github.com>
2023-08-04 10:00:57 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember()
+{
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var member = document.getElementById("member");
+
+    member.style.display = "block";
+    skills.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+    about.style.display = "none";
+    home.style.display = "none";
+}
